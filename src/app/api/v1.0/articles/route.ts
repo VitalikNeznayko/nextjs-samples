86d1493f5c5cde78fdd5923c065000871c9c2335
Vitaliy Neznayko <vitalik.neznayko1231@gmail.com>
2026-04-08 15:20:47 +0300

    some changes

--- src/app/api/v1.0/articles/route.ts
+++ src/app/api/v1.0/articles/route.ts
@@ -2,7 +2,7 @@ import { fetchAllPosts } from '@/server/data/fetcher';
 import {getArticles} from "@/server/db/article/getArticles";
 
 export async function GET() {
-  const articles = process.env.DB_DATABASE_URL ? await getArticles() : await fetchAllPosts();
+  const articles = await getArticles();
 
   return Response.json({
     server_welcome: process.env.ENV_WELCOME,
