9e7dd51b98fa0ecb5540eb21bbc1eb45fc4fba14
Vitaliy Neznayko <vitalik.neznayko1231@gmail.com>
2026-04-08 14:47:41 +0300

    some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
-import { Header } from '@/ui/components/common/Header/Header';
-import { Menu } from '@/ui/components/common/Menu/Menu';
-import { Logo } from '@/ui/components/common/Logo/Logo';
-import React from 'react';
+"use client";
+import { Header } from "@/ui/components/common/Header/Header";
+import { Menu } from "@/ui/components/common/Menu/Menu";
+import { Logo } from "@/ui/components/common/Logo/Logo";
+import React, { useEffect } from "react";
 
-export default async function Home() {
+export default function Home() {
+  useEffect(() => {
+    console.log(process.env.NEXT_PUBLIC_SOME_KEY);
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Header />
       <Logo />
-      <p>{process.env.ENV_WELCOME}</p>
-      <Menu/>
+      <Menu />
     </main>
   );
 }
